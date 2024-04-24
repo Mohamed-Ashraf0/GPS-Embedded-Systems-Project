c3356ea1494d2b40910cbf726bce528ac1f14ff5
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-04-24 15:45:50 +0200

    Update GPS.h

--- First Milestone Delivery/GPS.h
+++ First Milestone Delivery/GPS.h
@@ -1,1 +1,6 @@
-
+#ifndef GPS_H_
+#define GPS_H_
+double GPS_convertToRad(double degValue); 
+double GPS_getDistance(double currentLat,double currentLong,double previousLat,double previousLong);
+void GPS_read (); 
+#endif
