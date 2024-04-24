ef7b0275e9abd13e5a9f40681d0da178be365601
Abdelrahmant26 <68470744+Abdelrahmant26@users.noreply.github.com>
2024-04-24 22:20:36 +0200

    Update DIO.h
    Added required functions.

--- First Milestone Delivery/DIO.h
+++ First Milestone Delivery/DIO.h
@@ -1,1 +1,13 @@
+#ifndef DIO.h
+#define DIO.h
 
+#include "tm4c123gh6pm.h"
+void enableClock(type port){}
+void enableLEDs(){}
+void ledOFF(){}
+void greenON(){}
+void blueON(){}
+void redON(){}
+void SW(){}
+
+#endif
