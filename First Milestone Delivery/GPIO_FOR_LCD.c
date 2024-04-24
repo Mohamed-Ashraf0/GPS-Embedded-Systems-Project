bf82fd2449d135ab0b8da8e251ddff945596f8fe
muhammedkh45 <127145833+muhammedkh45@users.noreply.github.com>
2024-04-24 18:55:35 +0200

    Create GPIO_FOR_LCD.c

---
+++ First Milestone Delivery/GPIO_FOR_LCD.c
@@ -0,0 +1,46 @@
+/*
+name : mohammed khalid mohamed
+id : 2101103
+*/
+#include <stdint.h>
+#include"Bit_Utilies.h"
+#include "TM4C123.h"
+
+void GPIO_initPortA(){
+    SET_BIT(SYSCTL_RCGCGPIO_R,0);
+    while(GET_BIT(SYSCTL_PRGPIO_R,0)==0);
+    GPIO_PORTA_LOCK_R=GPIO_LOCK_KEY;
+    SET(GPIO_PORTA_CR_R,0xFF);
+    GPIO_PORTA_AMSEL_R=0x0;
+    SET(GPIO_PORTA_DIR_R,0xE0);
+    SET(GPIO_PORTA_DEN_R,0xE0);
+}
+void GPIO_initPortB(){
+    SET_BIT(SYSCTL_RCGCGPIO_R,1);
+    while(GET_BIT(SYSCTL_PRGPIO_R,1)==0);
+    GPIO_PORTF_LOCK_R=GPIO_LOCK_KEY;
+    SET(GPIO_PORTF_CR_R,0xFF);
+    CLR(GPIO_PORTF_AMSEL_R,0xF);
+    SET(GPIO_PORTF_DIR_R,0x13); 
+    SET(GPIO_PORTF_DEN_R,0x13);}
+
+void GPIO_initPortD(){
+
+SET_BIT(SYSCTL_RCGCGPIO_R,3); // BIT 3
+while(GET_BIT(SYSCTL_PRGPIO_R,3)== 0);
+GPIO_PORTD_LOCK_R= GPIO_LOCK_KEY;
+SET(GPIO_PORTD_CR_R,0xFF);
+CLR(GPIO_PORTD_AMSEL_R,0x0); // Disable Analog
+SET(GPIO_PORTD_DIR_R,0x07); // PD OutPut
+SET(GPIO_PORTD_DEN_R,0x07); // Enable digital
+
+}
+void GPIO_initPortE_(){
+    SET_BIT(SYSCTL_RCGCGPIO_R,4);
+    while(GET_BIT(SYSCTL_PRGPIO_R,4)==0);
+    GPIO_PORTF_LOCK_R=GPIO_LOCK_KEY;
+    SET(GPIO_PORTF_CR_R,0xFF);
+    CLR(GPIO_PORTF_AMSEL_R,0x0);
+    SET(GPIO_PORTF_DIR_R,0x30); 
+    SET(GPIO_PORTF_DEN_R,0x30);
+}
