33c548441d9511fbc625b3afe416f0c1fc9d528b
hussam023 <168012540+hussam023@users.noreply.github.com>
2024-04-24 19:03:23 +0200

    Update Bit_Utilies.h

--- First Milestone Delivery/Bit_Utilies.h
+++ First Milestone Delivery/Bit_Utilies.h
@@ -1,1 +1,15 @@
+#ifndef BIT_H
+#define BIT_H
 
+#define GET_BIT(reg1,bit)   ((reg1>>bit)&1)
+#define GET_REG(reg)        (reg&0xff)
+
+#define SET(reg,val)        reg |= val
+#define SET_BIT(reg,val)    reg |= (1<<val)
+
+#define CLR(reg,val)        reg &=~val
+#define CLR_BIT(reg,val)    reg &=~(1<<val)
+
+#define CHECK_REG(reg1,reg2) ((reg1&reg2))
+
+#endif
