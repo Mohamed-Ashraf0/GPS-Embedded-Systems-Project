ce8f30f2b0bd75a520026cc51ee4283f2e27f3c0
Abdelrahmant26 <68470744+Abdelrahmant26@users.noreply.github.com>
2024-04-24 22:31:50 +0200

    Update DIO.h
    Added comments

--- First Milestone Delivery/DIO.h
+++ First Milestone Delivery/DIO.h
@@ -1,12 +1,20 @@
+/*
+  Name : Abdelrahman Taha Mohamed 
+  Code : 2100464
+*/
 #ifndef DIO.h
 #define DIO.h
 #include "tm4c123gh6pm.h"
 void enableClock(type port);
+//takes values (A --> F) to enable clock for their ports
 void enableLEDs();
+//configure built-in LEDs to work
 void ledOFF();
+//turns off LEDs
 void greenON();
 void blueON();
 void redON();
 void SW();
+//configures built-in switches to work
 
 #endif
