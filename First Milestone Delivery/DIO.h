5d4d601759cedb092ff60fd9b194c63a15c505d7
Abdelrahmant26 <68470744+Abdelrahmant26@users.noreply.github.com>
2024-04-24 22:24:43 +0200

    Update DIO.h
    Added required functions.

--- First Milestone Delivery/DIO.h
+++ First Milestone Delivery/DIO.h
@@ -1,10 +1,6 @@
 #ifndef DIO.h
 #define DIO.h
 #include "tm4c123gh6pm.h"
-
-enum port {A=1,B=2,C=4,D=8,E=16,F=32};
-typedef enum port type;
-
 void enableClock(type port);
 void enableLEDs();
 void ledOFF();
