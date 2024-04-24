ba7ad86d71d7a08d01df5c8b9066530095fa2563
Seifehab11-8 <160287704+Seifehab11-8@users.noreply.github.com>
2024-04-24 18:59:02 +0200

    Update UART.h

--- First Milestone Delivery/UART.h
+++ First Milestone Delivery/UART.h
@@ -1,1 +1,12 @@
+#ifndef UART_H
+#define UART_H
+#include <stdint.h>
+#define BAUD_9600_RATE_INT (int)((16*1000000)/(9600*16))
+#define BAUD_9600_RATE_DIV (int)((BAUD_9600_RATE_INT - (int)(BAUD_9600_RATE_INT))*64 + 0.5)
+//#define CHAR_noPARITY_STP1 0x00000070
 
+void UART2_Init();
+char UART2_recieveByte(void);
+int UART2_receiveString(char *Str );
+
+#endif
