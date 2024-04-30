54c895e4a92a11f5d0787519b0f7a28fedc98ac7
AbdelrahmanAshour1111 <167990227+AbdelrahmanAshour1111@users.noreply.github.com>
2024-04-30 18:26:20 +0300

    Update SYSTICK.c

--- Test1/SYSTICK.c
+++ Test1/SYSTICK.c
@@ -1,27 +1,22 @@
-#include<stdint.h>
 #include "TM4C123.h"
-#include"Bit_Utilies.h"
+#include "SysTick.h"
 
-void SysTick_Init(void){
-NVIC_ST_CTRL_R = 0; // 1) disable SysTick during setup 
-NVIC_ST_RELOAD_R = 0x00FFFFFF; // 2) maximum reload value 
-NVIC_ST_CURRENT_R = 0; // 3) any write to CURRENT clears it 
-NVIC_ST_CTRL_R = 0x00000005; // 4) enable SysTick with core clock
+void SYSTICK_Init(void){
+	NVIC_ST_CTRL_R=0;
+	NVIC_ST_RELOAD_R=0X00FFFFFF;
+	NVIC_ST_CURRENT_R=0;
+	NVIC_ST_CTRL_R=0X05;	
 }
-// The delay parameter is in units of the 80 MHz core clock(12.5 ns)
-void SysTick_Wait(uint32_t delay){
-NVIC_ST_RELOAD_R = delay-1; 
-NVIC_ST_CURRENT_R = 0;
-// number of counts
-// any value written to CURRENT clears
-while((NVIC_ST_CTRL_R&0x00010000)==0){ // wait for flag
+void SYSTICK_WAIT(uint32_t delay){
+	NVIC_ST_RELOAD_R=delay-1;
+	NVIC_ST_CURRENT_R=0;
+	while((NVIC_ST_CTRL_R&0x00010000)==0){
+	}
 }
-}
-// Call this routine to wait for delay*1ms 
-void SysTick_Wait1ms(uint32_t delay){ 
+void SysTick_Wait10ms(uint32_t delay){
 unsigned long i;
-for(i=0; i<delay; i++){ 
-SysTick_Wait(80000); // wait 1ms
+for(i=0; i<delay; i++){
+SYSTICK_WAIT(800000); // wait 10ms
 }
 }
 
