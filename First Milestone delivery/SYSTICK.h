fa565b12ef60088ebab1a6fa1ebdfde0d625f88a
AbdelrahmanAshour1111 <167990227+AbdelrahmanAshour1111@users.noreply.github.com>
2024-04-30 17:34:01 +0300

    Update SYSTICK.h

--- First Milestone delivery/SYSTICK.h
+++ First Milestone delivery/SYSTICK.h
@@ -1,1 +1,4 @@
+#define NVIC_ST_CTRL_R(*((volatile uint32_t *)0xE000E010))
+#define NVIC_ST_RELOAD_R(*((volatile uint32_t *)0xE000E014)) 
+#define NVIC_ST_CURRENT_R(*((volatile uint32_t *)0xE000E018))
 
