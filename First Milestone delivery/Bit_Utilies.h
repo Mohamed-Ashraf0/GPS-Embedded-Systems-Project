64a417b0907b0930fee5660d53bd464a6da69778
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-05-02 13:00:33 +0300

    Update Bit_Utilies.h

--- First Milestone delivery/Bit_Utilies.h
+++ First Milestone delivery/Bit_Utilies.h
@@ -1,1 +1,11 @@
 
+#ifndef BIT_H
+#define BIT_H
+
+#define GET_BIT(reg1,bit)   ((reg1>>bit)&1)
+#define GET_REG(reg)        (reg&0xff)
+
+#define SET(reg,val)        reg |= val
+#define SET_BIT(reg,val)    reg |= (1<<val)
+
+#define CLR(reg,val)        reg
