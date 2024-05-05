abffaf139d7ee828482bf8f2e81da9e380297bc2
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-05-05 19:41:54 +0300

    Update GPS.h

--- First Milestone delivery/GPS.h
+++ First Milestone delivery/GPS.h
@@ -1,6 +1,7 @@
 #ifndef GPS_H_
 #define GPS_H_
 double GPS_convertToRad(double degValue); 
-double computeDistance(double currentlat, currentlong, previouslat, previouslong);
-void GPS_read (); 
+//double GPS_getDistance(double currentLat,double currentLong,double previousLat,double previousLong);
+double computeDistance(double lat1, double long1, double lat2, double long2);
+void GPS_read (); 
 #endif
