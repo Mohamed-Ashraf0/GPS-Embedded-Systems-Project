667d754f4f9dc0d8c65c8aad017bc1e207b8e342
Abdelrahman Taha <68470744+Abdelrahmant26@users.noreply.github.com>
2024-05-05 19:47:23 +0300

    Update DIO.h

--- First Milestone delivery/DIO.h
+++ First Milestone delivery/DIO.h
@@ -3,9 +3,11 @@
   Code : 2100464
   Section : 3 CSE
 */
-#ifndef DIO.h
-#define DIO.h
+#ifndef DIO_h_
+#define DIO_h_
 #include "TM4C123.h"
+enum port {A=1,B=2,C=4,D=8,E=16,F=32};
+typedef enum port type;
 void enableClock(type port);
 //takes values (A --> F) to enable clock for their ports
 void enableLEDs();
