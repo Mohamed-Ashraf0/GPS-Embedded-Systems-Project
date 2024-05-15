5e26791a73053eb5e278750ad54142237c774768
Mohamed-Ashraf0 <160188301+Mohamed-Ashraf0@users.noreply.github.com>
2024-05-15 23:32:15 +0300

    Delete Second Milestone/GPS.h

--- Second Milestone/GPS.h
+++
@@ -1,6 +0,0 @@
-#ifndef GPS_H_
-#define GPS_H_
-double GPS_convertToRad(double degValue); 
-double computeDistance(double currentlat, currentlong, previouslat, previouslong);
-void GPS_read();
-#endif
