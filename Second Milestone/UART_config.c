48c148663cb848f595855b3259ffd6be83109bf7
Seifehab11-8 <160287704+Seifehab11-8@users.noreply.github.com>
2024-05-15 18:10:34 +0300

    Update UART_config.c

--- Second Milestone/UART_config.c
+++ Second Milestone/UART_config.c
@@ -1,9 +1,11 @@
 #include "UART_config.h"
 #include "Bit_Utilies.h"
 #include <stdint.h>
+#include <string.h>
 
 #include "TM4C123.h"
-
+char x;
+char y;
 void uint8_UART_config (UART_config_t * uartConfigNum) {
     CLR_BIT((uartConfigNum -> CTL) , 0);
 
@@ -18,7 +20,14 @@ void uint8_UART_config (UART_config_t * uartConfigNum) {
 
 uint8_t struct_UART_recieveByte(UART_config_t * uartConfigNum) {
     while(uartConfigNum ->FR & UART_FR_RXFE){}
-    return (uint8_t)(uartConfigNum->DR & 0xFF);
+			x=(uint8_t)(uartConfigNum->DR & 0xFF);
+    return x;
+}
+
+void void_UART_sendByte(uint8_t data, UART_config_t * uartConfigNum) {
+    while(uartConfigNum ->FR & UART_FR_TXFF){}
+			y=data;
+    (uartConfigNum -> DR) = data;
 }
 
 int8_t UART_receiveString(char *Str, UART_config_t * uartConfigNum)
@@ -33,4 +42,13 @@ int8_t UART_receiveString(char *Str, UART_config_t * uartConfigNum)
     }
     Str[i] = '\0';
     return 1;
+}
+/* break character is used to identify each string sepratly ex: '\n' */
+void UART_sendString(char* str, uint8_t breakChar, UART_config_t * uartConfigNum)
+{
+    uint8_t counter = 0;
+    for(counter = 0; counter < strlen(str); counter ++) {
+        void_UART_sendByte(str[counter], uartConfigNum);
+    }
+    void_UART_sendByte(breakChar, uartConfigNum);
 }
